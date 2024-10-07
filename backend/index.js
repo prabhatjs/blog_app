1dcb4c9bc571b35fdbd35d545b63188472502159
Prabhat Singh Yadav <prabhatmca18.jim@gmail.com>
2024-10-08 02:44:27 +0530

    mongodb atlas user sign up page

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,23 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const cors=require('cors');
+const dotenv=require('dotenv').config();
+
 
 const app=express();
+app.use(cors({}));
+// console.log(app.get("env"));
 /*
 app. use --> It is generally used for introducing middlewares in your application, 
 and express.json() is a built-in middleware function in Express.js that parses 
 incoming requests with JSON payloads and is based on the body-parser middleware.
 */
 app.use(express.json());
-
+// console.log(process.env);
 /** Database connection function for mongodb */
 async function databaseConnection(){
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/myblogdb');
+        await mongoose.connect(process.env.DATABASE_URL);
         console.log('DB connect');
     } catch (error) {
         console.log('error comes from mongodb connection');
@@ -20,6 +25,128 @@ async function databaseConnection(){
     }
 }
 
+//crate schema of users---------
+
+const userSchema=new mongoose.Schema({
+    name:String,
+    password:String,
+    email:{
+       type:String,
+        required:true,
+        unique:true,
+    }
+});
+
+//create model for this schema
+
+const User=mongoose.model("User",userSchema);
+
+app.post("/users",async (req,res)=>{
+    const {name,password,email} = req.body;
+    console.log(req.body);
+    try {
+        //check email password and email 
+        if(!name){
+            return res.status(400).json({
+                success:false,
+                message:'Please enter the name'
+            })
+        }if(!password){
+            return res.status(400).json({
+                success:false,
+                message:'Please enter the password'
+            });
+        }if(!email){
+            return res.status(400).json({
+                success:false,
+                message:'Please enter the Email'
+            });
+        }
+        //check for email in db or not
+        const emailAlreadyexist=await User.findOne({email})
+        if(emailAlreadyexist){
+            return res.status(400).json({
+                success:false,
+                message:'user already exist with this email ',
+            })
+        }
+
+        const newUser=await User.create({
+            name,
+            email,
+            password
+        });
+        //create user and send back response
+        return res.status(200).json({
+            success:true,
+            message:"User Created successfully",
+            newUser
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:'Please Try Again',
+            error:error.message
+        })
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//server creation 
 app.listen(3000,()=>{
     console.log("server started");
     databaseConnection();
